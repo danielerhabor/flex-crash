68e92099c442e263583674b99795cfb999c85c02
Daniel Erhabor <danielnerhabor@gmail.com>
2022-05-26 11:21:35 -0400

    Initialized TS React, created simple flex container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.flexContainer}>
+      <div className={styles.item}>Item 1</div>
+      <div className={styles.item}>Item 2</div>
+      <div className={styles.item}>Item 3</div>
     </div>
   );
-}
+};
 
 export default App;
