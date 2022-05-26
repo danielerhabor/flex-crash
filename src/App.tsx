e3032e26cc10d5c28de334682124826407eb39e2
Daniel Erhabor <danielnerhabor@gmail.com>
2022-05-26 12:51:19 -0400

    Used the `flex` property shorthand for `flex-shrink` `flex-grow` and `flex-basis`

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ const App = () => {
         <div className={styles.item}>Item 1</div>
         <div className={styles.item}>Item 2</div>
         <div className={styles.item}>Item 3</div>
-
+        <div className={styles.item}>Item 4</div>
+        <div className={styles.item}>Item 5</div>
       </div>
     </>
   );
